7fd2b30baf6e6889d06894be137234b288e2b847
DVs07 <aldopablo7@hotmail.com>
2024-05-06 18:35:29 -0300

    Se agregaron las alertas a cada campo del formulario.

--- js/app.js
+++ js/app.js
@@ -7,29 +7,31 @@ document.addEventListener('DOMContentLoaded', function() {
     const formulario = document.querySelector('#formulario');
 
     // Asignar eventos
-    inputEmail.addEventListener('blur', capturarDatos);
-    inputAsunto.addEventListener('blur', capturarDatos);
-    inputMensaje.addEventListener('blur', capturarDatos);
-    function capturarDatos(e) {
+    inputEmail.addEventListener('blur', validar);
+    inputAsunto.addEventListener('blur', validar);
+    inputMensaje.addEventListener('blur', validar);
+    function validar(e) {
         //console.log('Capturando datos...');
         //console.log(e.target.value);
+        // console.log(e.target.id); // ID del input, email, asunto, etc.
+        // console.log(e.target.parentElement);
         if(e.target.value.trim() === '')  { // trim() elimina los espacios en blanco. 
             // console.log('Este campo esta vacio!!!');
-            mostrarAlerta();
+            mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
         }else {
             console.log('Hay algo en el campo');
         }
     }
 })
 
-function mostrarAlerta(){
+function mostrarAlerta(mensaje, referencia){
     // console.log('Hubo un error!!!');
     // Generar alerta de error en HTML
     const error = document.createElement('p');
-    error.textContent = 'Hubo un error';
+    error.textContent = mensaje;
     // console.log(error);
     error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center');
 
     // Inyectar el error al formulario
-    formulario.appendChild(error);
+    referencia.appendChild(error);
 }
