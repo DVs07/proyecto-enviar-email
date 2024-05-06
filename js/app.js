96b3fcde296c2c09d13a892b64b2f2c6994fddf2
DVs07 <aldopablo7@hotmail.com>
2024-05-06 17:52:30 -0300

    Se agrego un mensaje en caso de que un campo se encuentre vacio(se lo visualiza al final del formulario). Se arreglo la captura de espacios en blanco en los inputs, ya no son tomados como datos.

--- js/app.js
+++ js/app.js
@@ -4,16 +4,32 @@ document.addEventListener('DOMContentLoaded', function() {
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
+    const formulario = document.querySelector('#formulario');
 
     // Asignar eventos
-    inputEmail.addEventListener('blur', function(e) {
-        console.log(e.target.value); // Obtener el valor del input.
-    });
-    inputAsunto.addEventListener('blur', function(e) {
-        console.log(e.target.value); // Obtener el valor del input.
-    });
-    inputMensaje.addEventListener('blur', function(e) {
-        console.log(e.target.value); // Obtener el valor del input.
-    });
-
+    inputEmail.addEventListener('blur', capturarDatos);
+    inputAsunto.addEventListener('blur', capturarDatos);
+    inputMensaje.addEventListener('blur', capturarDatos);
+    function capturarDatos(e) {
+        //console.log('Capturando datos...');
+        //console.log(e.target.value);
+        if(e.target.value.trim() === '')  { // trim() elimina los espacios en blanco. 
+            // console.log('Este campo esta vacio!!!');
+            mostrarAlerta();
+        }else {
+            console.log('Hay algo en el campo');
+        }
+    }
 })
+
+function mostrarAlerta(){
+    // console.log('Hubo un error!!!');
+    // Generar alerta de error en HTML
+    const error = document.createElement('p');
+    error.textContent = 'Hubo un error';
+    // console.log(error);
+    error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center');
+
+    // Inyectar el error al formulario
+    formulario.appendChild(error);
+}
