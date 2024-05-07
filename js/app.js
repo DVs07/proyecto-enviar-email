6f5d679498dd922f67d92657373e122e81960114
DVs07 <aldopablo7@hotmail.com>
2024-05-07 17:28:18 -0300

    Se corrigio la alerta del campo email, se agrego una expresion regular para validar que sea correcto.

--- js/app.js
+++ js/app.js
@@ -18,18 +18,21 @@ document.addEventListener('DOMContentLoaded', function() {
         if(e.target.value.trim() === '')  { // trim() elimina los espacios en blanco. 
             // console.log('Este campo esta vacio!!!');
             mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
-        }else {
-            console.log('Hay algo en el campo');
+            return;
         }
+        
+        if(e.target.id === 'email' && !validarEmail(e.target.value)){
+            mostrarAlerta('Email no valido', e.target.parentElement);
+            return;
+        }
+        limpiarAlerta(e.target.parentElement);
+        // console.log('Despues del if');
     }
 })
 
 function mostrarAlerta(mensaje, referencia){
-    // Comprobar si ya existe una alerta
-    const alerta = referencia.querySelector('.bg-red-600');
-    if(alerta){
-        alerta.remove();
-    }
+    limpiarAlerta(referencia);
+    
     // console.log('Hubo un error!!!');
     // Generar alerta de error en HTML
     const error = document.createElement('p');
@@ -39,4 +42,20 @@ function mostrarAlerta(mensaje, referencia){
 
     // Inyectar el error al formulario
     referencia.appendChild(error);
+}
+
+function limpiarAlerta(referencia){
+    // console.log('Desde limpiar alerta');
+    // Comprobar si ya existe una alerta
+    const alerta = referencia.querySelector('.bg-red-600');
+    if(alerta){
+        alerta.remove();
+    }
+}
+
+function validarEmail(email) {
+    const regex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+    const tester = regex.test(email);
+    // console.log(tester);
+    return tester;
 }
