d1966ee9470a7630355559523695d780d4ef304e
DVs07 <aldopablo7@hotmail.com>
2024-05-08 16:48:41 -0300

    Se agregaron y corrigieron las interacciones con el boton enviar.Se creo una funcionalidad que muestra un spinner que resetea el formulario(emulando el envio del email).

--- js/app.js
+++ js/app.js
@@ -13,24 +13,19 @@ document.addEventListener('DOMContentLoaded', function() {
     const formulario = document.querySelector('#formulario');
     const btnSubmit = document.querySelector('#formulario button[type="submit"]');
     const btnReset = document.querySelector('#formulario button[type="reset"]');
+    const spinner = document.querySelector('#spinner');
 
     // Asignar eventos
     inputEmail.addEventListener('input', validar);
     inputAsunto.addEventListener('input', validar);
     inputMensaje.addEventListener('input', validar);
 
+    formulario.addEventListener('submit', enviarEmail);
+
     btnReset.addEventListener('click', function(e){
         e.preventDefault();
 
-        // Resetear el objeto email
-        email.asunto = '';
-        email.email = '';
-        email.mensaje = '';
-
-        // Resetear el formulario
-        formulario.reset();
-
-        comprobarEmail();
+        resetFormulario();
     })
     // Funciones
     function validar(e) {
@@ -98,11 +93,38 @@ document.addEventListener('DOMContentLoaded', function() {
         if(Object.values(email).includes('')){
             btnSubmit.classList.add('opacity-50');
             btnSubmit.disabled = true;
+            btnSubmit.classList.add('btn-locked');
             return;
         }
         btnSubmit.classList.remove('opacity-50');
         btnSubmit.disabled = false;
-        
+        btnSubmit.classList.remove('btn-locked');
+    }
+
+    function enviarEmail(e) {
+        e.preventDefault();
+
+        // console.log('Enviando Email...');
+        spinner.classList.remove('hidden');
+
+        setTimeout(() => {
+
+            spinner.classList.add('hidden');
+
+            resetFormulario();
+        }, 3000);
+    }
+
+    function resetFormulario() {
+        // Resetear el objeto email
+        email.asunto = '';
+        email.email = '';
+        email.mensaje = '';
+
+        // Resetear el formulario
+        formulario.reset();
+
+        comprobarEmail();
     }
 })
 
